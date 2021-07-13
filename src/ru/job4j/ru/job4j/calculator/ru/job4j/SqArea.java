c95f305be387109e1558215f666a15f368d11a36
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-13 13:20:51 +0300

    9. Площадь треугольника. [#175474]

--- src/ru/job4j/ru/job4j/calculator/ru/job4j/SqArea.java
+++ src/ru/job4j/ru/job4j/calculator/ru/job4j/SqArea.java
@@ -4,8 +4,7 @@ public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double L = h * k;
-        double rsl = L * h;
-        return rsl;
+        return L * h;
     }
 
     public static void main(String[] args) {
