1936a17d2654562b24497d827f4d4561ae8328d8
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-13 15:45:17 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+       String idea = "I like Java!";
+       int year = 38;
+       System.out.println(idea + " But I am a newbie.");
+       System.out.println(idea +  year);
+    }
+}
--- src/ru/job4j/ru/job4j/calculator/ru/job4j/StudentInfo.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ru.job4j.calculator.ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Yekutiel Katanov");
-    }
-}
