1c2e503fc94c3ec9b7d6cc752d649579a21018a9
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-13 12:20:40 +0300

    7. Расстояние между точками в системе координат [#188 #177072]

--- src/ru/job4j/ru/job4j/calculator/ru/job4j/Point.java
+++ src/ru/job4j/ru/job4j/calculator/ru/job4j/Point.java
@@ -4,10 +4,10 @@ public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         double a = x2 - x1;
         double b = y2 - y1;
-        double c = Math.pow(double a);
-        double d = Math.pow(double b);
+        double c = Math.pow(a,2);
+        double d = Math.pow(b,2);
         double e = c + d;
-        double rsl = Math.sqrt(double e);
+        double rsl = Math.sqrt(e);
         return rsl;
     }
     public static void main(String[] args) {
---
+++ src/ru/job4j/ru/job4j/calculator/ru/job4j/SqArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.ru.job4j.calculator.ru.job4j;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double rsl = -1;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
