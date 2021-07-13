f655e7d994249cfa65a90becb8851ffd14fc4e5f
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-13 17:36:00 +0300

    9.2. Checkstyle [#4123 #177259]

--- src/ru/job4j/Greeting.java
+++ src/ru/job4j/Greeting.java
@@ -2,9 +2,11 @@ package ru.job4j;
 
 public class Greeting {
     public static void main(String[] args) {
-       String idea = "I like Java!";
-       System.out.println(idea + " But I am a newbie.");
-       int year = 2021;
-       System.out.println(idea + year);
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But i am a newbie.";
+        int year = 2021;
+        idea += year;
+        System.out.println(idea);
     }
 }
