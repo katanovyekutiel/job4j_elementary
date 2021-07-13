4b057797e3c70c37c37f0a8cbeb209b17fbe6c3c
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-13 17:19:34 +0300

    9.1. String [#237407 #177238]

--- src/ru/job4j/Greeting.java
+++ src/ru/job4j/Greeting.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class Greeting {
     public static void main(String[] args) {
        String idea = "I like Java!";
-       int year = 38;
        System.out.println(idea + " But I am a newbie.");
-       System.out.println(idea +  year);
+       int year = 2021;
+       System.out.println(idea + year);
     }
 }
