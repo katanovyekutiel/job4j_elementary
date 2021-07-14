f21384061c5e011cdd76fa0508e026b25b94ec88
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-14 23:35:14 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

---
+++ src/ru/job4j/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height1) {
+        return (height1 - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short height = 180;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 180 is " + man + " perfect weight.");
+
+        short height1 = 170;
+        double woman = Fit.womanWeight(height1);
+        System.out.println("Woman 170 is " + woman + " perfect weight.");
+    }
+}
---
+++ test/ru/job4j/FitTest.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        short in = 170;
+        double expected1 = 69;
+        double out1 = Fit.womanWeight(in);
+        Assert.assertEquals(expected1, out1, 0.01);
+    }
+}
