1bc60e7f5a3937e1f369b6ae24e093a8abf46355
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-15 10:05:00 +0300

    1.1. Что такое import [#504773 #177739]

--- src/ru/job4j/math/MathCalculator.java
+++ src/ru/job4j/math/MathCalculator.java
@@ -1,6 +1,6 @@
 package ru.job4j.math;
 
-import ru.job4j.math.MathFunction;
+import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
@@ -12,12 +12,15 @@ public class MathCalculator {
     }
 
     public static double sumAndMultiplyAndDifferenceAndDivision(double first, double second) {
-        return MathFunction.sum(first, second) + MathFunction.multiply(first, second) + MathFunction.difference(first, second) + MathFunction.division(first, second);
+        return sum(first, second) + multiply(first, second)
+                + difference(first, second) + division(first, second);
     }
 
     public static void main(String[] args) {
         System.out.println("Результат рфсчетов: " + sumAndMultiply(10, 20));
-        System.out.println("Результат суммы: разностя и деления: " + differenceAndDivision(10, 20));
-        System.out.println("Результат суммы всех четырех операций: " + sumAndMultiplyAndDifferenceAndDivision(10, 20));
+        System.out.println("Результат суммы: разностя и деления: "
+                + differenceAndDivision(10, 20));
+        System.out.println("Результат суммы всех четырех операций: "
+                + sumAndMultiplyAndDifferenceAndDivision(10, 20));
     }
 }
--- src/ru/job4j/math/MathFunction.java
+++ src/ru/job4j/math/MathFunction.java
@@ -16,5 +16,4 @@ public class MathFunction {
     public static double division(double first, double second) {
         return first / second;
     }
-
 }
