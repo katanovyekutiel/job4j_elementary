b8e65cbb480fe557e620cdf6aa0508f41b918231
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-19 01:10:20 +0300

    4.3.5. Множественное логическое выражение И [#9461]

---
+++ src/ru/job4j/ChessBoard.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if ((x1 != x2 && y1 != y2) && (x1 < 7 && x2 < 7 && y1 < 7 && y2 < 7)) {
+            rsl = Math.abs(x2 - x1);
+            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        }
+        return rsl;
+    }
+}
