1a3b22801a5b36fde3ceb6e996fa1f8197ab4d58
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-20 23:15:16 +0300

    5.6. Простые числа [#156314 #179904]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+import java.security.PublicKey;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(5));
+        System.out.println(calc(11));
+        System.out.println(calc(2));
+    }
+}
