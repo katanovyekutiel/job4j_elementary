092ab88e11afd85c5a014f9c1ea3b24f42ffd165
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-21 10:10:16 +0300

    5.7. Ипотека [#156315 #179915]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,12 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        for (year = 1; salary <= amount * percent; year++) {
-          amount = (amount * percent) - salary;
-          year++;
-            if (amount * percent <= salary) {
-                break;
-            }
+        while (amount > 0) {
+            amount += amount * percent / 100;
+            amount -= salary;
+            year++;
         }
         return year;
     }
